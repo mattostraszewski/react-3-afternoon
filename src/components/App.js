6fb470ea6bff7e8600ba1c2bcbf5419902576b5d
mattostraszewski <mrostraszewski@gmail.com>
2020-05-21 13:05:38 -0600

    need help

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
+import Post from './Post/Post'
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+
 
 class App extends Component {
   constructor() {
@@ -13,25 +15,31 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+
   }
-  
+
   componentDidMount() {
-
+    axios.get('https://practiceapi.devmountain.com/api/posts').then(results => {
+      this.setState({ ports: results.data })
+    })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) { //why am i passing in id and text. how does that bring us closer to out end goal? Explain the URL please.
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text }).then(results => {
+      this.setState({ posts: results.data });
+    });
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`).then(results => {
+      this.setState({ posts: results.data });
+    });
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post('https://practiceapi.devmountain.com/api/posts', { text }).then(results => {
+      this.setState({ posts: results.data });
+    });
   }
 
   render() {
@@ -43,8 +51,15 @@ class App extends Component {
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose createPostFn={this.createPost} />
+
+          {
+            posts.map(elem => (
+              <Posts key={elem.id} text={elem.text} date={elem.date} id={post.id}
+                updatePostFn={this.updatePost} deletePostFn={this.deletePost} /> //Please explain what this is doing? 
+            ))
+          }
+
         </section>
       </div>
     );
